26868e064509a525bde08d7309813c9333e8968e
Shuvo Paul <shuvopaul@yahoo.com>
2026-09-10 11:15:01 +0600

    chore(profile): make bn local name required

--- config/alumkit.php
+++ config/alumkit.php
@@ -66,7 +66,7 @@ return [
     ],
 
     'local_names' => [
-        'bn' => ['label' => 'বাংলা নাম', 'required' => false],
+        'bn' => ['label' => 'বাংলা নাম', 'required' => true],
     ],
 
 ];
