9430621a37e2bae44a8158b01424a72b42df06c9
Shuvo Paul <shuvopaul@yahoo.com>
2026-09-16 13:08:37 +0600

    feat(models): move User model into package as base class
    Apps now extend Alumkit\Alumkit\Models\User instead of wiring
    traits individually. Base model ships verification, 2FA, roles,
    notifications, and profile relations out of the box.
    
    README updated with the extend contract and rationale.

---
+++ src/Models/User.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alumkit\Alumkit\Models;
+
+use Alumkit\Alumkit\Traits\HasCareers;
+use Alumkit\Alumkit\Traits\HasEducations;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Spatie\Permission\Traits\HasRoles;
+
+/**
+ * @property string $state
+ */
+class User extends Authenticatable implements MustVerifyEmailContract
+{
+    use HasCareers;
+    use HasEducations;
+    use HasRoles;
+    use MustVerifyEmail;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+
+    /** @var list<string> */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'password',
+        'state',
+    ];
+
+    /** @var list<string> */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes',
+    ];
+
+    /** @return array<string, string> */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
